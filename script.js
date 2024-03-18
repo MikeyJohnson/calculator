81e310ac258e821be60cdbf684b382ea9a65e531
MikeyJohnson <159175556+MikeyJohnson@users.noreply.github.com>
2024-03-18 14:31:18 -0400

    Styled Buttons, JavaScript generated HTML, removed old HTML

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const calculator = document.getElementById('Calculator');
+const calculator = document.getElementById('calculator');
 let display = document.getElementById("display");
 // calculator.innerHTML = 'calculator'
 display.innerHTML = 5;
 // screen
+
 //button 1-9
+for(let i = 1; i < 10; i++){
+    let button = document.createElement("button");
+   button.innerHTML = i;
+   button.classList.add("calc-button");
+
+   button.addEventListener("click",function(){
+    display.innerHTML += i;
+   })
+   
+   calculator.appendChild(button);
+   
+}
 //operators
 // = button
+
 
 // functionality
 let result = 0;
