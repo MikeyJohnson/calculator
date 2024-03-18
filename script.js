a5b8b5f0eff61b01ac0261f083000bda477477b4
MikeyJohnson <159175556+MikeyJohnson@users.noreply.github.com>
2024-03-18 14:03:24 -0400

    started Calculator Project

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const calculator = document.getElementById('Calculator');
+let display = document.getElementById("display");
+// calculator.innerHTML = 'calculator'
+display.innerHTML = 5;
+// screen
+//button 1-9
+//operators
+// = button
+
+// functionality
+let result = 0;
+
+console.log(result);
